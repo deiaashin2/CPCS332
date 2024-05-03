ab0ed35dde22e754c6416f51e7b648e596e4daf5
Thais Shin <thaishin@Thaiss-MacBook-Air.local>
2024-05-03 10:59:25 -0700

    Fix event page

--- event.php
+++ event.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="eventdesign.css">
     <title>Document</title>
 </head>
+
 <body>
 
     <br>
     <br>
-    
+
     <div class="cont">
         <div class="form sign-in">
             <h2>Create Event</h2>
             <form action="eventback.php" method="post">
-            <label>
-                <span>Event Name</span>
-                <input type="text" name="e_name" placeholder="">
-            </label>
-            <label>
-                <span>Description</span>
-                <input type="text" name="description" placeholder="">
-            </label>
-            <label>
-                <span>Max Capacity</span>
-                <input type="text" name="max_capacity" placeholder="">
-            </label>
-            <label>
+                <label>
+                    <span>Event Name</span>
+                    <input type="text" name="e_name" placeholder="">
+                </label>
+                <label>
+                    <span>Description</span>
+                    <textarea rows="10" cols="30" type="text" name="description" placeholder="">
+                </textarea>
+                </label>
+                <label>
+                    <span>Max Capacity</span>
+                    <input type="text" name="max_capacity" placeholder="">
+                </label>
+                <label>
                     <span>Address</span>
-                    <input type="text" name="address" placeholder="">
+                    <textarea rows="5" cols="30" type="text" name="address" placeholder="">
+                </textarea>
                 </label>
                 <label>
                     <span>Event Type</span>
@@ -43,17 +47,17 @@
                     <span>Event Type</span>
                     <input type="text" name="e_type" placeholder="">
                 </label>
-                <button>Create</button>                    
-                </form>
+                <button type="submit" class="submit">Create Event</button>
+            </form>
         </div>
     </div>
 
 
     <script>
-        document.querySelector('.img__btn').addEventListener('click', function() {
+        document.querySelector('.img__btn').addEventListener('click', function () {
             document.querySelector('.cont').classList.toggle('s--signup');
         });
     </script>
 </body>
-</html>
 
+</html>
