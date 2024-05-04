fb760e5d2722d7e50eacb5cfab8c9492a152ddab
Paul Lujan <paul.lujan.contractor@hilton.com>
2024-05-04 16:31:10 -0700

    added database

--- eventback.php
+++ eventback.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         define("DB_SERVER", "localhost");
         define("DB_USER", "root");
-        define("DB_PWD", "");
+        define("DB_PWD", "123");
         define("DB_NAME", "aem");
 
         $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PWD, DB_NAME);
